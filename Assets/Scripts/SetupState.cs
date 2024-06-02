1f5139789bdc74084e3e55316223c862efaf4d7f
SoultatosStefanos <stefanoss1498@gmail.com>
2024-06-02 11:42:51 +0300

    WIP FSMs

---
+++ Assets/Scripts/SetupState.cs
@@ -0,0 +1,12 @@
+public class SetupState : GameState
+{
+    public override void OnEnter()
+    {
+        // TODO enable/disable controllers/scripts
+    }
+
+    public override void OnExit()
+    {
+        // TODO enable/disable controllers/scripts
+    }
+}

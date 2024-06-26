f73119a2b1f1f553e866ed365fcac97af788d8c0
SoultatosStefanos <stefanoss1498@gmail.com>
2024-06-26 23:27:08 +0300

    Todos

---
+++ Assets/Scripts/Presenters/AuctionBiddingActionsPresenter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Presenters
+{
+    // TODO On AuctionTurnChangedEvent do
+    // foreach btn in { levelButtons, denomButtons, passButton, doubleButton}
+    // check if the corresponding play with btn can be played (and all other combinations if it's level or denom)
+    // activate/deactivate accordingly
+    // use tags for the buttons
+
+    public class AuctionBiddingActionsPresenter : MonoBehaviour
+    {
+    }
+}

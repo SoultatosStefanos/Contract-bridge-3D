7f414595af089777dafb2676b0871b3e6d8ff4a8
SoultatosStefanos <stefanoss1498@gmail.com>
2024-07-02 21:29:04 +0300

    Fixed AuctionCallController

---
+++ Assets/Scripts/Wrappers/DenominationWrapper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Wrappers
+{
+    public class DenominationWrapper : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Wrappers/LevelWrapper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Wrappers
+{
+    public class LevelWrapper : MonoBehaviour
+    {
+        
+    }
+}
